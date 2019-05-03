c327652511bf915508afae9b978eb8ad460cb0e9
Yaroslav Nedilko <yavned@gmail.com>
2019-05-03 22:58:35 +0300

    Update InquiryController.Get

--- Project2C2P/Controllers/InquiryController.cs
+++ Project2C2P/Controllers/InquiryController.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Project2C2P.Models;
+using Project2C2P.Services;
 
 namespace Project2C2P.Controllers
 {
@@ -7,10 +10,37 @@ namespace Project2C2P.Controllers
     [ApiController]
     public class InquiryController : ControllerBase
     {
+        private IInquiryService _service;
+
+        public InquiryController(IInquiryService service)
+        {
+            _service = service;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Get(long? customerID, string email)
         {
-            return Ok();
+            if (customerID != null)
+            {
+                InquiryView result = await _service.GetByCustomerID((long)customerID);
+
+                if (result != null)
+                    return Ok(result);
+
+                return NotFound();
+            }
+
+            if (!String.IsNullOrEmpty(email))
+            {
+                InquiryView result = await _service.GetByemail(email);
+
+                if (result != null)
+                    return Ok(result);
+
+                return NotFound();
+            }
+
+            return BadRequest("No inquiry criteria");
         }
 
     }
